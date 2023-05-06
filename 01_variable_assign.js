0fc6174516eed5535385114f8749d57eee5676e6
aishwaryagandhile2019 <aishwaryagandhile2019@gmail.com>
2023-05-06 01:25:39 -0700

    variable assign code file
    variable assign code file

---
+++ 01_variable_assign.js
@@ -0,0 +1,37 @@
+
+ var myFullName;
+ console.log(myFullName);
+
+ var myFullName
+ console.log("Aishwarya Gandhile");
+
+ var myFullName
+ console.log("Aishwarya Anil Gandhile");
+ var myLove
+ console.log("I Love only JS");
+
+ var firstName="Aishwarya";
+ console.log(firstName);
+
+ var lastName="Gandhile";
+ console.log(lastName);
+
+ var age="22"
+ console.log(age);
+
+ var address="Vadgaon Ghenand";
+ console.log(address);
+
+ var pincode="410501"
+ console.log(pincode);
+
+ var gender="Female"
+ console.log(gender);
+
+ var isMarried="No"
+ console.log(isMarried);
+
+
+
+
+ 
