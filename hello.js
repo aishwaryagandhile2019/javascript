b5667a1cb91523e8e16fa9aab98d37dcbf9bacca
aishwaryagandhile2019 <aishwaryagandhile2019@gmail.com>
2023-05-06 01:27:06 -0700

    hello js file
    hello js file

---
+++ hello.js
@@ -0,0 +1,30 @@
+
+
+
+
+console.log("Aishwarya Gandhile..");
+
+var firstName = "Aishwarya";
+console.log(firstName);
+
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
+
+console.log("I love Js");
+
+
+
+
+
+
+
+
