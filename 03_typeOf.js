25cb4c51b93c12258d7d75efecec762a6f471ab5
aishwaryagandhile2019 <aishwaryagandhile2019@gmail.com>
2023-05-06 01:26:38 -0700

    03_typeOf js code file
    03_typeOf js code file

---
+++ 03_typeOf.js
@@ -0,0 +1,27 @@
+
+
+var bankName;
+console.log("bankName:",bankName,"type:",typeof bankName);
+bankName="SBI Bank";
+console.log("bankName:",bankName, " type:", typeof bankName);
+
+console.log("--------------------------");
+
+var marks='90%';
+console.log("marks:",marks, " Data type:",typeof marks);
+
+console.log("--------------------------");
+
+var isWorking;
+console.log("isWorking:",isWorking," type before initilization;",typeof isWorking );
+isWorking=true;
+console.log("isWorking:",isWorking," type after initilization:",typeof isWorking);
+
+console.log("---------------------------")
+
+
+var totalCount="Hundred and Seven";
+console.log("totalCount:",totalCount ,"type:",typeof totalCount);
+totalCount=107;
+console.log("totalCount:",totalCount,"type of variable totalCount:",typeof totalCount);
+
