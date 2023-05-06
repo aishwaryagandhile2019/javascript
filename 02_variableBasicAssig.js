482c3cc3f7bea160956e3e53c0df2c63cab6078a
aishwaryagandhile2019 <aishwaryagandhile2019@gmail.com>
2023-05-06 01:26:11 -0700

    variableBasic code file
    variableBasic code file

---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,44 @@
+
+
+
+var firstName="Bill";
+console.log("first name:",firstName ,"type:",typeof firstName);
+
+var lastName="Gandhile";
+console.log("last Name:",lastName, "type:",typeof lastName);
+
+var card="gita";
+console.log("card:",card, "type:",typeof card);
+var favCountry="South Korea";
+console.log("favCountry:",favCountry,"Data Type:",typeof favCountry)
+console.log("-----------*------------")
+
+
+var number=3456;
+console.log("number:",number,"type:",typeof number);
+
+var PIN=410501;
+console.log("PIN:",PIN,"type:",typeof PIN);
+
+var ATM=234;
+console.log("ATM:",ATM,"type:",typeof ATM);
+console.log("-----------*-------------")
+
+
+var machine=true;
+console.log("machine:" ,machine,"type:",typeof machine);
+
+var sensor=false;
+console.log("sensor:" ,sensor,"type:",typeof sensor);
+
+var code=true;
+console.log("code:" ,code,"type:",typeof code);
+
+console.log("------------------Difference Between undefined and null Data type----------")
+
+
+console.log("undefined-->undefined means variable is declare but value is not Assigned" )
+console.log("null means-->null means variable is declare but value is assigned as null value and null means empty value and primitive data type");
+
+
+
