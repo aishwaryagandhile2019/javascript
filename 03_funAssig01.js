30f1f736c374f030ebfaa87e5f1b3d378ba05d91
aishwaryagandhile2019 <aishwaryagandhile2019@gmail.com>
2023-05-10 23:59:35 -0700

    function assignment of js
    function assignment of js

---
+++ 03_funAssig01.js
@@ -0,0 +1,47 @@
+
+console.log("--------------Q1-------------")
+function show(){
+  console.log("Hii My Name is Aishwarya...");
+}
+show();
+
+function display() {
+    console.log("I Have Completed my computer Science Graducation in MIT ACSC COLLEGE ALANDI.");
+    
+}
+display();
+console.log("|||||||||-------Q2--------||||||||||");
+console.log("---------------------------------------");
+
+function personalDetails(firstName,lastName,collegeName){
+    console.log("firstName:",firstName);
+    console.log("lastName:",lastName);
+    console.log("collegeName:",collegeName);
+}
+personalDetails("Aishwarya","Gandhile","MIT ACSC College");
+
+console.log("|||||||||||-----------Q3----------|||||||||");
+console.log("-------------------------------------------");
+
+function swapValueDude(arg1,arg2){
+    console.log("before swap:",arg1,arg2);
+    var temp=arg1;
+    arg1=arg2;
+    arg2=temp;
+    console.log("After Swap:",arg1,arg2);
+}
+swapValueDude("virat","Anushka");
+swapValueDude("1000","2000");
+console.log("|||||||||||-----------Q4----------|||||||||");
+console.log("-------------------------------------------");
+
+
+function addThreeValues(a,b,c){
+    var Add=a+b+c;
+    console.log("Addition of three values and String:",Add);
+
+}
+addThreeValues(10.23,600,40);
+addThreeValues("Hello","Good","Morning");
+
+
