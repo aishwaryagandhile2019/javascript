8501816fce69dd25e122cdacce1553ef2dd83c35
aishwaryagandhile2019 <aishwaryagandhile2019@gmail.com>
2023-05-18 23:49:55 -0700

    grade Assignmnet
    grade Assignmnet

---
+++ 06_grade.js
@@ -0,0 +1,57 @@
+function gradeCalculation(marks){
+    
+    if (marks==undefined || isNaN(marks) || marks<0 || marks>100) { // undefined, null
+        console.log(`Invalid marks: ${marks}`);
+    } else {
+       console.log(`Valid marks : ${marks}`); 
+       if (marks>=90) {
+            console.log(`Fantastic marks : ${marks}, Your grade is A+`);
+       } else {
+        if(marks>=75 && marks<=90){
+            console.log(`Excellent marks ${marks},your grade is A`);
+        }
+        else {
+        if(marks>=50 && marks<=75) {
+            console.log(`Good marks ${marks},your grade is B`);
+            
+        } 
+        else{
+            if(marks>=35 && marks<50){
+                console.log(`marks is ${marks},your grade is C ,Need Improvement`);
+            }
+            else{
+                if(marks<0 || marks>100){
+                    console.log(`please provide the valid marks ${marks}`);s
+                }
+            
+            }
+        }
+            
+        }
+        
+       }
+    }
+    
+}
+// Handling invalid inputs
+gradeCalculation(undefined);
+gradeCalculation(null);
+gradeCalculation(NaN);
+
+gradeCalculation("Karthik");
+gradeCalculation(-10);
+gradeCalculation(120);
+gradeCalculation(90);
+gradeCalculation(0);
+gradeCalculation(98);
+gradeCalculation(80);
+gradeCalculation(150);
+gradeCalculation(-7);
+gradeCalculation(35);
+gradeCalculation(29);
+gradeCalculation(64);
+gradeCalculation(49);
+gradeCalculation("91");
+gradeCalculation("Eighty");
+// Valid input
+gradeCalculation("20");
