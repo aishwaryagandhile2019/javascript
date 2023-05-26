6bdd7132672f4efe1e002591f60936b7741b54a6
aishwaryagandhile2019 <aishwaryagandhile2019@gmail.com>
2023-05-26 00:21:03 -0700

    06_while Assignmnet
    06_while Assignmnet

---
+++ 06_while_Assignment.js
@@ -0,0 +1,62 @@
+
+
+
+console.log(`Q1 Print the no from 5 to 15 by incrementing 1`);
+let i=5;
+while(i<=15){
+    console.log(i);
+    i=i+1;
+}
+
+console.log(`Q2.print the no from 50 to 40 by decrementing by 1`);
+let index=50;
+while(index>=40){
+     console.log(index);
+     index=index-1;
+}
+
+
+
+
+console.log(`Q3.Find the first 10 even no:`);
+let j=0;  // initialization
+while (j<=18) {  // condition
+   if (j%2==0) {
+      console.log(j);
+   }
+   j++; // update expression
+}
+
+console.log(`Q4.Find the first 15 odd no:`);
+let k=1;
+while(k<=30){
+    if(k%2!==0){
+       console.log(k);
+    }
+    k++;
+}
+
+
+console.log(`Q5.print table of 5 like--> 5 10 15 20 25.....50`)
+let p=5;
+while(p<=50){
+    console.log(p);
+    p=p+5;
+}
+
+console.log(`Q6.print table of 10 like 10 20 30 40.....100`);
+let a=10;
+while(a<=100){
+    console.log(a);
+    a=a+10;
+}
+
+
+console.log(`Q7.print table of 10 in reverse order like 100 90 80 70 ...10`);
+let s=100;
+while(s>=10){
+    console.log(s);
+    s=s-10;
+}
+
+
